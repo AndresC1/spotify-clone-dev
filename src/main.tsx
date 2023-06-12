d21fab4776022a34d0d5d0d420cf4c153cdbfc87
AndresC1 <andrescornejolira@gmail.com>
2023-06-12 07:09:34 -0600

    main layout implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/home'
+import Home from './pages/Home/home'
 import { RouterProvider, createBrowserRouter, } from 'react-router-dom'
 import 'tailwindcss/tailwind.css'
+import MainLayout from './components/layouts/mainLayout'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />
+    element: <MainLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
   }
 ])
 
