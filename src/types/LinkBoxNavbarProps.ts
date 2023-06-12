497fd1aabcf874206d0eb11428cd4a05c972f860
AndresC1 <andrescornejolira@gmail.com>
2023-06-12 07:07:01 -0600

    LinkBoxNavbar interface

---
+++ src/types/LinkBoxNavbarProps.ts
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+interface LinkBoxNavbarProps {
+    ruta: string;
+    name: string;
+    iconDisabled: ReactElement;
+    iconActived: ReactElement;
+    id: number;
+}
+
+export default LinkBoxNavbarProps
