4b19eb5ea6501961b83f9f6d507e01447e250e07
AndresC1 <andrescornejolira@gmail.com>
2023-06-11 22:38:55 -0600

    react-router-dom implementation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home'
+import { RouterProvider, createBrowserRouter, } from 'react-router-dom'
 import 'tailwindcss/tailwind.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
